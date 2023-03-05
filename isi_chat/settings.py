5a143164b238f2ff4a255999580c912816551f0d
Yaroslav Demchenko <dastex1411@gmail.com>
2023-03-05 10:34:05 +0200

    prepare settings to deploy

--- isi_chat/settings.py
+++ isi_chat/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
+import os
 from datetime import timedelta
 from pathlib import Path
 
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-^#hkh!(48v53x*4h2sorz3vu63jindl_bfj-9sgg+5k5y=iyn5"
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
